0e51f920086c7e54f9e79ba006768ad0620618a9
DinhPhucTran <trandinhphuc95@gmail.com>
2016-05-24 22:45:03 +0700

    Initializing project

---
+++ EnglishCenter/View/LoginWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EnglishCenter.View
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //App.Current.Shutdown();
+            this.Close();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            if (tbUsername.Text == "dp" && tbPass.Password == "abc") {
+                mainWindow.Show();
+                this.Close();
+            }                
+            else
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng");
+            
+        }
+    }
+}
