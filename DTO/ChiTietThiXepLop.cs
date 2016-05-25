9f49c203f958c89e8a71aade9c4ea41893f4cb42
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-25 17:12:29 +0700

    add more DTO class

---
+++ DTO/ChiTietThiXepLop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class ChiTietThiXepLop
+    {
+        String mMaThiXepLop;
+
+        public String MMaThiXepLop
+        {
+            get { return mMaThiXepLop; }
+            set { mMaThiXepLop = value; }
+        }
+        String mMaHocVien;
+
+        public String MMaHocVien
+        {
+            get { return mMaHocVien; }
+            set { mMaHocVien = value; }
+        }
+        float mKetQuaThi;
+
+        public float MKetQuaThi
+        {
+            get { return mKetQuaThi; }
+            set { mKetQuaThi = value; }
+        }
+        String mChuongTrinhDeNghi;
+
+        public String MChuongTrinhDeNghi
+        {
+            get { return mChuongTrinhDeNghi; }
+            set { mChuongTrinhDeNghi = value; }
+        }
+        String mChuongTrinhMongMuon;
+
+        public String MChuongTrinhMongMuon
+        {
+            get { return mChuongTrinhMongMuon; }
+            set { mChuongTrinhMongMuon = value; }
+        }
+
+
+    }
+}
---
+++ DTO/DeThi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class DeThi
+    {
+        String mMaDeThi;
+
+        public String MMaDeThi
+        {
+            get { return mMaDeThi; }
+            set { mMaDeThi = value; }
+        }
+        String mLoaiDeThi;
+
+        public String MLoaiDeThi
+        {
+            get { return mLoaiDeThi; }
+            set { mLoaiDeThi = value; }
+        }
+        String mChiTiet;
+
+        public String MChiTiet
+        {
+            get { return mChiTiet; }
+            set { mChiTiet = value; }
+        }
+
+    }
+}
