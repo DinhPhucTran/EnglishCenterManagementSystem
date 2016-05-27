261fb3ae2ce4c1bf76a4c0ace0db2982b533c07f
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-27 15:13:57 +0700

    add insertdata

---
+++ InsertData.sql
@@ -0,0 +1,54 @@
+﻿use QL_ANHNGU
+
+insert into Ca values('0','7:00','8:30')
+insert into Ca values('1','9:00','10:30')
+insert into Ca values('2','13:00','14:30')
+insert into Ca values('3','15:00','16:30')
+insert into Ca values('4','17:00','18:30')
+insert into Ca values('5','19:00','20:30')
+
+
+Insert into TRINH_DO values('BEGIN','Beginner')
+Insert into TRINH_DO values('ELEM','Elementary')
+Insert into TRINH_DO values('PRE','Pre-Intermediate')
+Insert into TRINH_DO values('INTER','Intermediate')
+Insert into TRINH_DO values('UPPER','Upper-Intermediate')
+Insert into TRINH_DO values('ADV','Advanced')
+
+
+Insert into CHUONG_TRINH_HOC values('IEL.BE','IELTS for Beginner','BEGIN',1,2)
+Insert into CHUONG_TRINH_HOC values('TOE.BE','TOEIC for Beginner','BEGIN',0,150)
+Insert into CHUONG_TRINH_HOC values('IEL.EL','IELTS for Elementary','ELEM',2,3)
+Insert into CHUONG_TRINH_HOC values('TOE.EL','TOEIC for Elementary','ELEM',151,300)
+Insert into CHUONG_TRINH_HOC values('IELT.PR','IELTS Pre-intermediate','PRE',3,4)
+Insert into CHUONG_TRINH_HOC values('TOEF.PR','TOEFL Pre-intermediate','PRE',0,31)
+Insert into CHUONG_TRINH_HOC values('TOEI.PR','TOEIC Pre-intermediate','PRE',301,400)
+
+Insert into CHUONG_TRINH_HOC values('IELT.INT','IELTS Intermediate','INTER',4,5)
+Insert into CHUONG_TRINH_HOC values('TOEF.INT','TOEFL Intermediate','INTER',31,34)
+Insert into CHUONG_TRINH_HOC values('TOEI.INT','TOEIC Intermediate','INTER',401,525)
+
+Insert into CHUONG_TRINH_HOC values('IELT.UP','IELTS upper-intermediate','UPPER',5,6)
+Insert into CHUONG_TRINH_HOC values('TOEF.UP','TOEFL upper-intermediate','UPPER',35,59)
+Insert into CHUONG_TRINH_HOC values('TOEI.UP','TOEIC upper-intermediate','UPPER',526,750)
+
+Insert into CHUONG_TRINH_HOC values('IELT.AD','IELTS Advanced','ADV',6,7)
+Insert into CHUONG_TRINH_HOC values('TOEF.AD','TOEFL Advanced','ADV',60,93)
+Insert into CHUONG_TRINH_HOC values('TOEI.AD','TOEIC Advanced','ADV',751,900)
+
+
+
+Insert into THU values('T2',N'Thứ 2')
+Insert into THU values('T3',N'Thứ 3')
+Insert into THU values('T4',N'Thứ 4')
+Insert into THU values('T5',N'Thứ 5')
+Insert into THU values('T6',N'Thứ 6')
+Insert into THU values('T7',N'Thứ 7')
+Insert into THU values('CN',N'Chủ Nhật')
+
+
+Insert into GIANG_VIEN values ('1','Phan Nguyệt Minh','Quận 1 TPHCM','0969696969')
+Insert into GIANG_VIEN values ('2','Lê Thanh Trọng','Quân 2 TPHCM','0969696969')
+Insert into GIANG_VIEN values ('3','Nguyễn Thị Nhơn','Quân 3 TPHCM','0969696969')
+Insert into GIANG_VIEN values ('4','Trần Đình Phúc','Quận 4 TPHCM','0969696969')
+
