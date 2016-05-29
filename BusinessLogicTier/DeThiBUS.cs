aeacbdbcf1badd6b20994c8689949a2010dd6ca9
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-29 18:18:49 +0700

    DeThi BUS DAO

---
+++ BusinessLogicTier/DeThiBUS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DataAccessTier;
+
+namespace BusinessLogicTier
+{
+    public class DeThiBUS
+    {
+        public DeThiBUS() { }
+
+        public List<DeThi> getListDeThi()
+        {
+            return new DeThiDAO().getListDeThi();
+        }
+
+        public bool themDeThi(DeThi dth)
+        {
+            return new DeThiDAO().themDeThi(dth);
+        }
+
+        public bool xoaDeThi(String ma)
+        {
+            return new DeThiDAO().xoaDeThi(ma);
+        }
+
+        public bool suaDeThi(DeThi dethi)
+        {
+            return new DeThiDAO().suaDeThi(dethi);
+        }
+    }
+}
