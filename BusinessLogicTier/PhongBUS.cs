6af1dac215c7c9305bdfe4bba3fd415f8bd4e3da
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-29 18:19:19 +0700

    Phong BUS DAO

---
+++ BusinessLogicTier/PhongBUS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DataAccessTier;
+
+namespace BusinessLogicTier
+{
+    public class PhongBUS
+    {
+        public PhongBUS()
+        {
+
+        }
+        public List<Phong> getListPhong()
+        {
+            return new PhongDAO().getListPhong();
+        }
+
+        public bool suaPhong(Phong p)
+        {
+            return new PhongDAO().suaPhong(p);
+        }
+
+        public bool themPhong(Phong p)
+        {
+            return new PhongDAO().themPhong(p);
+        }
+        public bool xoaPhong(String maPhong){
+            return new PhongDAO().xoaPhong(maPhong);
+        }
+    }
+}
