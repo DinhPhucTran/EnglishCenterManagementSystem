5d31eca7a7a4f3cbd6a23c2639c237bfb6d58dca
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-30 15:39:51 +0700

    add ThoiGIanHOc DAO BUS

---
+++ BusinessLogicTier/ThoiGianHocBUS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessTier;
+using DTO;
+
+namespace BusinessLogicTier
+{
+    public class ThoiGianHocBUS
+    {
+        public ThoiGianHocBUS() { }
+        public bool themThoiGianHoc(ThoiGianHoc tgh){
+            return new ThoiGianHocDAO().themThoiGianHoc(tgh);
+        }
+        public bool xoaThoiGianHoc(String maTgh)
+        {
+            return new ThoiGianHocDAO().xoaThoiGianHoc(maTgh);
+        }
+
+        public bool suaThoiGianHoc(ThoiGianHoc tgh)
+        {
+            return new ThoiGianHocDAO().suaThoiGianHoc(tgh);
+        }
+    }
+}
---
+++ DataAccessTier/ThoiGianHocDAO.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using System.Data.SqlClient;
+namespace DataAccessTier
+{
+    public class ThoiGianHocDAO :DBConnection
+    {
+        public ThoiGianHocDAO(){
+
+        }
+        public bool themThoiGianHoc(ThoiGianHoc tgh)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand command = new SqlCommand("THOI_GIAN_HOC_INSERT", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@MaLop", tgh.MMaLop);
+                command.Parameters.AddWithValue("@MaThu", tgh.MMaThu);
+                command.Parameters.AddWithValue("@MaCa", tgh.MMaCa);
+                command.ExecuteNonQuery();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                Console.WriteLine(ex);
+            }
+            return false;
+        }
+
+        public bool suaThoiGianHoc(ThoiGianHoc tgh)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand command = new SqlCommand("THOI_GIAN_HOC_UPDATE", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@MaLop", tgh.MMaLop);
+                command.Parameters.AddWithValue("@MaThu", tgh.MMaThu);
+                command.Parameters.AddWithValue("@MaCa", tgh.MMaCa);
+                command.ExecuteNonQuery();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                Console.WriteLine(ex);
+            }
+            return false;
+        }
+        public bool xoaThoiGianHoc(String maTg)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlCommand command = new SqlCommand("THOI_GIAN_HOC_UPDATE", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@MaLop", maTg);
+                command.ExecuteNonQuery();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                Console.WriteLine(ex);
+            }
+            return false;
+        }
+    }
+
+    
+}
